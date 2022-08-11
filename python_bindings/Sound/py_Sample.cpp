b703922bbec9f4f3a308061c7a2a1e1a1b456b3e
Ralf Waspe <rwaspe@icloud.com>
2022-08-12 00:26:34 +0200

    small changes

--- python_bindings/Sound/py_Sample.cpp
+++ python_bindings/Sound/py_Sample.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 #include <Sound/Sample.h>
 
@@ -6,10 +7,12 @@ void init_Sample(pybind11::module_& module)
 {
    pybind11::class_<Sample> sample(module, "Sample");
    sample.def(pybind11::init<>());
-   
-   pybind11::class_<Sample::Meta> sample_meta(sample, "Sample::Meta");
+
+   pybind11::class_<Sample::Meta> sample_meta(sample, "SampleMeta");
    sample_meta.def(pybind11::init<>());
-   
+   sample_meta.def_readwrite("noOfChannels", &Sample::Meta::noOfChannels);
+   sample_meta.def_readwrite("sampleRate", &Sample::Meta::sampleRate);
+   sample_meta.def_readwrite("numberOfSamples", &Sample::Meta::numberOfSamples);
 
    // autogen start
    sample.def_static("load", &Sample::load);
@@ -18,4 +21,3 @@ void init_Sample(pybind11::module_& module)
    sample.def_static("deinterlace", &Sample::deinterlace);
    // autogen end
 }
-
